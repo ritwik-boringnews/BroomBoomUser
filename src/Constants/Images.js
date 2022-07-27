e9ab4f18344947975d4ad1b029caa4a02a5b8e84
Souvik Bhattacharyya <57744600+Souvik-Bhattacharyya@users.noreply.github.com>
2022-07-27 17:41:58 +0530

    welcome

---
+++ src/Constants/Images.js
@@ -0,0 +1,9 @@
+export const Logo = require('../../assets/logoSmall.png');
+export const Card = require('../../assets/Card.png');
+export const Slider = require('../../assets/slideProgress.png');
+export default {
+    Logo,
+    Card,
+    Slider
+};
+  
--- src/Constants/metrics.js
+++ src/Constants/metrics.js
@@ -3,10 +3,10 @@ const { width, height } = Dimensions.get("window");
 const guidelineBaseWidth = 380;
 const guidelineBaseHeight = 815;
 
-const Metric = {
+const metrics = {
   horizontalScale: (size) => (width / guidelineBaseWidth) * size,
   verticalScale: (size) => (height / guidelineBaseHeight) * size,
   moderateScale: (size, factor = 0.25) =>
     size + (metrics.scale(size) - size) * factor,
 };
-export default Metric;
+export default metrics;
