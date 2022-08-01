ef6944ac22e1638bd50b210ad3c57b38c1d67d2c
SyedaFarhin <syedafarhin18@gmail.com>
2022-08-01 12:19:38 +0530

    ride details

---
+++ src/Pages/RideDetails/index.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const RideDetails = () => {
+  return (
+    <View>
+      <Text>RideDetails</Text>
+    </View>
+  );
+};
+
+export default RideDetails;
