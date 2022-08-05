e0ecd3097560e69dd93737f0472489ee3c31ba19
Ritwik <ritwik@boringnews.co>
2022-08-05 13:24:20 +0530

    map cred changed

--- src/Pages/GMapHome/index.js
+++ src/Pages/GMapHome/index.js
@@ -3,15 +3,11 @@ import React,{useState} from "react";
 import { StyleSheet, Text, View } from "react-native";
 import MapView,{Marker,PROVIDER_GOOGLE} from "react-native-maps";
 import MapViewDirections from 'react-native-maps-directions';
-const GOOGLE_MAPS_APIKEY = 'AIzaSyAkOoXEDWvsDoAvaQQ3EE36wP97BUf1eNw';
+const GOOGLE_MAPS_APIKEY = 'AIzaSyCkVARy-jUojHtiIxcu90g3heAEJDyhqrE';
 
 const styles = StyleSheet.create({
  container: {
-   ...StyleSheet.absoluteFillObject,
-   height: 400,
-   width: 400,
-   justifyContent: 'flex-end',
-   alignItems: 'center',
+   flex:2
  },
  map: {
    ...StyleSheet.absoluteFillObject,
@@ -21,20 +17,20 @@ const styles = StyleSheet.create({
 export default () => {
   
   const arambagh = {
-    latitude: 22.8764,
-    longitude: 87.7049,
+    latitude: 22.8765,
+    longitude: 87.7910,
     latitudeDelta: 0.01,
     longitudeDelta: 0.01,
   };
   const [region, setRegion] = useState(arambagh);
   const destination = {
-    latitude: 23.4232,
-    longitude: 88.3883,
+    latitude: 22.90,
+    longitude: 88.39,
     latitudeDelta: 0.01,
     longitudeDelta: 0.01,
   };
   return (
-    <View>
+    <View style={{flex:1}}>
      <View style={styles.container}>
      <MapView
         style={styles.map}
@@ -48,21 +44,22 @@ export default () => {
         apikey={GOOGLE_MAPS_APIKEY}
       />
       <Marker
-      key={1}
+      key={`marker${1}`}
       coordinate={arambagh}
       title="arambagh"
     />
     <Marker
-      key={1}
+      key={`marker${2}`}
       coordinate={destination}
       title="mayapur"
     />
   
       </MapView>
-   <Text style={styles.text}>Current latitude: {region.latitude}</Text>
-    <Text style={styles.text}>Current longitude: {region.longitude}</Text>
+ 
    </View>
-    
+    <View style={{flex:1}}>
+      <Text style={{}}>sayeda</Text>
+    </View>
    </View>
    )
 };
