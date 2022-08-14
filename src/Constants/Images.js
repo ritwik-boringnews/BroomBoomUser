9968063b94ce4ac090ead9d560559515711e1bdb
AbinashBasak <abinashbasak129@gmail.com>
2022-08-15 02:06:55 +0530

    update payment page

--- src/Constants/Images.js
+++ src/Constants/Images.js
@@ -1,8 +1,22 @@
 export const Logo = require('../../assets/logoSmall.png');
+export const AmazonLogo = require('../../assets/a.png');
 export const Card = require('../../assets/Card.png');
 export const Slider = require('../../assets/slideProgress.png');
+export const Cash = require('../../assets/cash.png');
+export const Google = require('../../assets/google.png');
+export const Paytm = require('../../assets/paytm.png');
+export const Qr = require('../../assets/Qr.png');
+export const Upi = require('../../assets/upi.png');
+export const ArrowLeft = require('../../assets/arrow-left.png');
 export default {
-    Logo,
-    Card,
-    Slider
+  Logo,
+  Card,
+  Slider,
+  AmazonLogo,
+  Cash,
+  Google,
+  Paytm,
+  Qr,
+  Upi,
+  ArrowLeft,
 };
