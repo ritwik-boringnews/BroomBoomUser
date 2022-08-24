f4a937e6fc3ad1b45028acf7dbac95c7f0b12243
SyedaFarhin <syedafarhin18@gmail.com>
2022-08-24 21:54:13 +0530

    api change

--- src/Services/index.js
+++ src/Services/index.js
@@ -1,7 +1,7 @@
 import API from './ApiServices';
 
-export const baseUrl = 'http://192.168.43.126:7000/api/v1';
-// export const baseUrl = "http://3.110.168.181:7000/api/v1";
+// export const baseUrl = 'http://192.168.43.126:7000/api/v1';
+export const baseUrl = 'http://43.205.135.42:8000/api/v1';
 
 const Api = new API({
   baseUrl: baseUrl,
